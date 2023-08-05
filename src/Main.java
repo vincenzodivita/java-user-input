2fdc6a8db6ac8681982688183ea43a4ac3c17a0b
vincenzodivita <divitavincenzo@icloud.com>
2023-08-05 16:23:41 +0200

    Fix comment

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Import Scanner
+        // Define Scanner method
         Scanner scanner = new Scanner(System.in);
 
         // Questions
